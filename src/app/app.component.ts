be6da5a3264c2314fe51d1bcfcde8cae9de05064
yqni13 <65848806+yqni13@users.noreply.github.com>
2021-08-11 23:06:53 +0200

    added darkmode toggle + design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,31 @@ import {LoginService} from './login.service';
 export class AppComponent {
   title = 'fh-tracking-app';
   loggedin: boolean= false;
+  themeCounter = 0;
 
   constructor(private loginService: LoginService) {
     this.loginService.loggedin$.subscribe(x => this.loggedin = x);
   }
-
+  
   loggout(){    
     this.loginService.logOut();
+  }
+  colorTheme() {
+    ++this.themeCounter;
+    if (this.themeCounter%2 == 1) {
+      // change to darkmode
+      document.body.style.background = "var(--darkmode)";
+      document.getElementsByTagName('h1')[0].style.color = "var(--logoyellow)";
+      document.getElementById('toggle_theme')!.style.color = "var(--logoyellow)";
+    } else {
+      // change to lightmode
+      document.body.style.background = "var(--lightmode)";
+      document.getElementsByTagName('h1')[0].style.color = "var(--buttonback)";
+      document.getElementById('toggle_theme')!.style.color = "var(--buttonback)";
+    }
+    console.log(this.themeCounter);
+  }
+  
+  ngOnInit() {
   }
 }
