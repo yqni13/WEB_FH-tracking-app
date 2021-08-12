54961256e3629e6846adf2944edf3e3906dc7659
yqni13 <65848806+yqni13@users.noreply.github.com>
2021-08-12 20:47:37 +0200

    added dummy data to DB

--- demo_InMemoryDB/db.js
+++ demo_InMemoryDB/db.js
@@ -14,9 +14,18 @@ module.exports.db = {
     tokens: [],
     
     trackingRecords: [ 
-        { username: "tobi1", room: "A0.0", date: "01.04.2021", begin: "08:00:00", end: "09:30:00", timeInSeconds:  5400},
-        { username: "yqni13", room: "A2.09", date: "03.05.2021", begin: "08:00:00", end: "11:10:00", timeInSeconds: 11400},
-        { username: "yqni13", room: "A3.01", date: "06.05.2021", begin: "09:40:00", end: "11:10:00", timeInSeconds: 5400 }
+        { username: "tobi1", room: "BR_F7.04", date: "01.04.2021", begin: "08:00:00", end: "09:30:15", timeInSeconds:  5415},
+        { username: "yqni13", room: "EDV_A5.10", date: "03.05.2021", begin: "08:00:00", end: "11:10:49", timeInSeconds: 11449},
+        { username: "alf1", room: "Bro_F6.04", date: "04.05.2021", begin: "08:00:00", end: "11:45:12", timeInSeconds: 13512 },
+        { username: "yqni13", room: "EDV_A2.06", date: "05.05.2021", begin: "09:40:00", end: "11:10:03", timeInSeconds: 5403 },
+        { username: "yqni13", room: "EDV_A6.08", date: "06.05.2021", begin: "12:50:00", end: "14:19:07", timeInSeconds: 5347 },
+        { username: "yqni13", room: "EDV_E0.11", date: "10.05.2021", begin: "16:00:00", end: "17:39:22", timeInSeconds: 5362 },
+        { username: "yqni13", room: "EDV_F1.01", date: "12.05.2021", begin: "08:00:00", end: "11:10:49", timeInSeconds: 10849 },
+        { username: "yqni13", room: "EDV_A6.08", date: "13.05.2021", begin: "12:50:00", end: "14:22:38", timeInSeconds: 5558 },
+        { username: "yqni13", room: "EDV_E1.07", date: "17.05.2021", begin: "16:00:00", end: "17:44:31", timeInSeconds: 5671 },
+        { username: "yqni13", room: "EDV_A2.06", date: "19.05.2021", begin: "09:40:00", end: "11:10:17", timeInSeconds: 5417 },
+        { username: "yqni13", room: "HS_A3.14", date: "26.05.2021", begin: "09:40:00", end: "10:41:08", timeInSeconds: 3668 },
+        { username: "yqni13", room: "HS_A1.04B", date: "27.05.2021", begin: "12:05:00", end: "13:10:56", timeInSeconds: 3959 },
     ],
     
     register: function(userName, firstName, lastName, email, password) {
